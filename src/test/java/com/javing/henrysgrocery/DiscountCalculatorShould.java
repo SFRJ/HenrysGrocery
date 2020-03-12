5b39a6c1eaf01ab77b72a80e54839c10757138b7
djordje <me@mymail.com>
2020-03-12 13:40:09 +0000

    Adding more tests to discount calculator(duplicating the ones that live in Basket.class)

--- src/test/java/com/javing/henrysgrocery/DiscountCalculatorShould.java
+++ src/test/java/com/javing/henrysgrocery/DiscountCalculatorShould.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import java.time.LocalDate;
 
 import static java.time.LocalDate.now;
+import static java.time.temporal.TemporalAdjusters.lastDayOfMonth;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class DiscountCalculatorShould {
@@ -55,5 +56,50 @@ public class DiscountCalculatorShould {
         Double price = discountCalculator.price("soup", "soup", "bread");
 
         assertThat(price).isEqualTo(1.7D);
+    }
+
+    @Test
+    public void applesPromotionStartsAfterThreedays() {
+
+        LocalDate purchaseDateWithinPromotion = now().plusDays(4);
+        discountCalculator = new DiscountCalculator(purchaseDateWithinPromotion);
+
+        Double price = discountCalculator.price("apples");
+
+        assertThat(price).isEqualTo(0.09D);
+    }
+
+    @Test
+    public void applesPromotionValidUntilLastDayOfNextMonth() {
+
+        LocalDate purchaseDateNotInPromotion = now().plusMonths(1).with(lastDayOfMonth());
+
+        discountCalculator = new DiscountCalculator(purchaseDateNotInPromotion);
+
+        Double price = discountCalculator.price("apples");
+
+        assertThat(price).isEqualTo(0.09D);
+    }
+
+    @Test
+    public void applesPromotionNotValidAfterLastDayOfNextMonth() {
+
+        LocalDate purchaseDateNotInPromotion = now().plusMonths(1).with(lastDayOfMonth()).plusDays(1);
+
+        discountCalculator = new DiscountCalculator(purchaseDateNotInPromotion);
+
+        Double price = discountCalculator.price("apples");
+
+        assertThat(price).isEqualTo(0.1D);
+    }
+
+    @Test
+    public void priceCorrectlyAVarietyOfProductsPurchasedInFiveDays() {
+        LocalDate purchaseDateWithinPromotion = now().plusDays(5);
+        discountCalculator = new DiscountCalculator(purchaseDateWithinPromotion);
+
+        Double price = discountCalculator.price("apples", "apples", "apples", "soup", "soup", "bread");
+
+        assertThat(price).isEqualTo(1.97D);
     }
 }
