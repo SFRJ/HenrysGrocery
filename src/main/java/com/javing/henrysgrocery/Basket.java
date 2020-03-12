719de73f54728fba127e81e2b95d7da440fd3b46
djordje <me@mymail.com>
2020-03-12 14:07:22 +0000

    Removed unused constructor to complete refactor

--- src/main/java/com/javing/henrysgrocery/Basket.java
+++ src/main/java/com/javing/henrysgrocery/Basket.java
@@ -1,23 +1,15 @@
 package com.javing.henrysgrocery;
 
-import java.time.LocalDate;
-
 import static com.javing.henrysgrocery.Item.getByName;
 import static java.lang.Double.parseDouble;
 import static java.lang.String.format;
 
 public class Basket {
 
-    private LocalDate purchaseDate;
     private DiscountCalculator discountCalculator;
 
     public Basket(DiscountCalculator discountCalculator) {
         this.discountCalculator = discountCalculator;
-    }
-
-    @Deprecated
-    public Basket(LocalDate purchaseDate) {
-        this.purchaseDate = purchaseDate;
     }
 
     public double price(String... items) throws IllegalArgumentException {
