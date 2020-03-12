44dce5069e4d044a55d8b4019aa8efbfe8254559
djordje <me@mymail.com>
2020-03-12 11:33:31 +0000

    refactor removing conditionals from loop to later extract method

--- src/main/java/com/javing/henrysgrocery/Basket.java
+++ src/main/java/com/javing/henrysgrocery/Basket.java
@@ -1,30 +1,25 @@
 package com.javing.henrysgrocery;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static com.javing.henrysgrocery.Item.getByName;
 import static java.lang.Double.parseDouble;
 import static java.lang.String.format;
+import static java.util.Arrays.stream;
 
 public class Basket {
 
     public double price(String... items) throws IllegalArgumentException {
 
         double total = 0D;
-        int soupCount = 0;
-        int breadCount = 0;
+        int soupCount = (int) stream(items).filter(i -> i.equals("soup")).count();
+        int breadCount = (int) stream(items).filter(i -> i.equals("bread")).count();
         double breadDiscounts = 0D;
 
         for (String item : items) {
 
-            if (item.equals("soup")) {
-                soupCount++;
-            }
-
-            if (item.equals("bread")) {
-                breadCount++;
-            }
-
             total += getByName(item).getPrice();
-
         }
 
         if (soupCount >= 2 && breadCount > 0) {
