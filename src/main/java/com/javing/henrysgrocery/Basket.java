f1c593599f619514f8d656a965507add7bec9174
djordje <me@mymail.com>
2020-03-12 09:32:28 +0000

    Refactoring to enum(wip)

--- src/main/java/com/javing/henrysgrocery/Basket.java
+++ src/main/java/com/javing/henrysgrocery/Basket.java
@@ -18,4 +18,27 @@ public class Basket {
 
         return parseDouble(format("%.2f", total));
     }
+
+    private enum Item {
+
+        SOUP("soup", 0.65),
+        BREAD("bread", 0.8);
+
+        private final String itemKey;
+        private final double price;
+
+        Item(String itemKey, double price) {
+
+            this.itemKey = itemKey;
+            this.price = price;
+        }
+
+        public Item getByName(String name) {
+            for (Item item : Item.values()) {
+                if(item.itemKey.equals(name))
+                    return item;
+            }
+            return null;
+        }
+    }
 }
