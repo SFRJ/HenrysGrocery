81bdb66a32274aa28b76441513ef18c6df70f255
djordje <me@mymail.com>
2020-03-12 13:01:42 +0000

    Adding wiring for terminal input and output

---
+++ src/main/java/com/javing/henrysgrocery/Application.java
@@ -0,0 +1,21 @@
+package com.javing.henrysgrocery;
+
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+        Basket basket = new Basket(LocalDate.now());
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to Henry's Grocery program");
+        System.out.println("The items you can add to the basket are: soup, milk, apples, bread");
+        System.out.println("Please enter a list of items separated by a blank space");
+
+        double total = basket.price(scanner.nextLine().split(" "));
+
+        System.out.println("The total price of the basket is: " + total);
+    }
+
+}
