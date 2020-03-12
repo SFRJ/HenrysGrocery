f01f32b3a69a1767c765b4c5824dc2634daa376a
djordje <me@mymail.com>
2020-03-12 14:04:51 +0000

    removed unused methods

--- src/main/java/com/javing/henrysgrocery/Basket.java
+++ src/main/java/com/javing/henrysgrocery/Basket.java
@@ -5,8 +5,6 @@ import java.time.LocalDate;
 import static com.javing.henrysgrocery.Item.getByName;
 import static java.lang.Double.parseDouble;
 import static java.lang.String.format;
-import static java.time.temporal.TemporalAdjusters.lastDayOfMonth;
-import static java.util.Arrays.stream;
 
 public class Basket {
 
@@ -31,47 +29,6 @@ public class Basket {
         }
 
         return parseDouble(format("%.2f", total - discountCalculator.discountBread(items) - discountCalculator.discountApples(items)));
-    }
-
-    private double discountApples(String[] items) {
-
-        int appleCount = countItem(items, "apples");
-
-        if(purchaseDate.isAfter(LocalDate.now().plusDays(3)) &&
-                purchaseDate.isBefore(LocalDate.now().plusMonths(1).with(lastDayOfMonth()).plusDays(1))) {
-            return appleCount * 0.01D;
-        } else {
-            return 0D;
-        }
-
-    }
-
-    private double discountBread(String[] items) {
-
-        double breadDiscounts = 0D;
-        int soupCount = countItem(items, "soup");
-        int breadCount = countItem(items, "bread");
-
-        LocalDate validFrom = LocalDate.now().minusDays(1);
-        LocalDate validTo = LocalDate.now().plusDays(6L);
-        boolean isValidDiscount = purchaseDate.isAfter(validFrom) && purchaseDate.isBefore(validTo);
-
-        if (soupCount >= 2 && breadCount > 0 && isValidDiscount) {
-
-            int applicableDiscounts = Double.valueOf(soupCount / 2D).intValue();
-
-            if (applicableDiscounts > breadCount) {
-                applicableDiscounts -= breadCount;
-            }
-
-            breadDiscounts = applicableDiscounts * 0.4;
-        }
-
-        return breadDiscounts;
-    }
-
-    private int countItem(String[] items, String itemName) {
-        return (int) stream(items).filter(i -> i.equals(itemName)).count();
     }
 
 }
